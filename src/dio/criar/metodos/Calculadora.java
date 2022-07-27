4d4510108ed5b5d2c3b2650198fa78e31236c9e4
anacarolina1 <lopesdasilva.ac@gmail.com>
2022-07-26 22:15:49 -0300

    exercício calculadora

---
+++ src/dio/criar/metodos/Calculadora.java
@@ -0,0 +1,34 @@
+package dio.criar.metodos;
+
+public class Calculadora {
+	
+	
+	public static void soma(double a, double b) {
+		
+		double resultado = a + b;
+		System.out.println("a soma de " + a + " + " + b + " é: " + resultado);
+		
+	}
+	
+	public static void subtracao(double a, double b) {
+		
+		double resultado = a - b;
+		System.out.println("a subtração de " + a + " - " + b + " é: " + resultado);
+		
+	}
+	
+	public static void multiplicacao(double a, double b) {
+		
+		double resultado = a * b;
+		System.out.println("a multiplicação de " + a + " x " + b + " é: " + resultado);
+		
+	}
+	
+	public static void divisao(double a, double b) {
+		
+		double resultado = a / b;
+		System.out.println("a divisão de " + a + " / " + b + " é: " + resultado);
+		
+	}
+
+}
